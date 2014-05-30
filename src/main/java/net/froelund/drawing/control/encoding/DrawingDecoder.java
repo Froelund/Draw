d6940d004653d5b8c634774549fb371a49b4552e
Kristian Frølund <kristian@froelund.net>
2014-05-30 10:38:10 +0200

    Added resistance to invalid json.

--- src/main/java/net/froelund/drawing/control/encoding/DrawingDecoder.java
+++ src/main/java/net/froelund/drawing/control/encoding/DrawingDecoder.java
@@ -12,25 +12,30 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.Collections;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by froelund on 5/28/14.
  */
 public class DrawingDecoder implements Decoder.TextStream<Drawing> {
 
+    private static final Logger logger = Logger.getLogger(DrawingDecoder.class.getName());
+
     @Override
-    public Drawing decode(Reader reader) throws DecodeException, IOException {
-
-        JsonReader jsonReader = Json.createReader(reader);
-        JsonObject jsonDrawing = jsonReader.readObject();
-
+    public Drawing decode(Reader reader) {
         Drawing drawing = new Drawing();
-        JsonObject drawingJsonData = jsonDrawing.getJsonObject("data");
-        if (drawingJsonData != null){
-            for (Map.Entry<String, JsonValue> entry : drawingJsonData.entrySet()){
-                JsonObject jsonObject = (JsonObject) entry.getValue();
-                drawing.getData().put(new DrawingPoint(entry.getKey()), new ColorInfo(jsonObject.getInt("r"), jsonObject.getInt("g"), jsonObject.getInt("b"), jsonObject.getInt("transparency")));
+        try(JsonReader jsonReader = Json.createReader(reader)){
+            JsonObject jsonDrawing = jsonReader.readObject();
+            JsonObject drawingJsonData = jsonDrawing.getJsonObject("data");
+            if (drawingJsonData != null){
+                for (Map.Entry<String, JsonValue> entry : drawingJsonData.entrySet()){
+                    JsonObject jsonObject = (JsonObject) entry.getValue();
+                    drawing.getData().put(new DrawingPoint(entry.getKey()), new ColorInfo(jsonObject.getInt("r"), jsonObject.getInt("g"), jsonObject.getInt("b"), jsonObject.getInt("transparency")));
+                }
             }
+        } catch (JsonException e){
+            logger.log(Level.INFO, "Invalid json pushed.");
         }
         return drawing;
     }
